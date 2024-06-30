4cd1aaf58eb894ffa460f41da5486576ca3411b7
Yashwantsheshkar <yashwantsheshkar@gmail.com>
2024-06-30 19:05:02 +0530

    Initial Commit

---
+++ InteractiveXiOS15/InteractiveXiOS15App.swift
@@ -0,0 +1,17 @@
+//
+//  InteractiveXiOS15App.swift
+//  InteractiveXiOS15
+//
+//  Created by Yashwant Sheshkar on 30/06/24.
+//
+
+import SwiftUI
+
+@main
+struct InteractiveXiOS15App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
